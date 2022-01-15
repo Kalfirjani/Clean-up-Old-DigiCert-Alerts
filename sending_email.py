26c8d9570cd4a2c0f0eb56b41ab74ccc1bb5facb
Kalfirjani <82188059+Kalfirjani@users.noreply.github.com>
2022-01-15 10:38:01 +0000

    Create sending_email.py

---
+++ sending_email.py
@@ -0,0 +1,25 @@
+import smtplib, ssl, email
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+def send_email(email_body):
+    sender_email = ""
+    receiver_email = ""
+    password = "yzlmumuxrrpcaopn"
+
+    #Create MIMEMultipart object
+    msg = MIMEMultipart("alternative")
+    msg["Subject"] = "multipart test"
+    msg["From"] = 'Kalfirjani@gmail.com'
+    msg["To"] = 'hajir.61291@gmail.com'
+    msg.set_content = email_body
+    msg.attach(MIMEText(email_body, 'html'))
+    # Create secure SMTP connection and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(
+            sender_email, receiver_email, msg.as_string()
+        )
